95153c3bd2f4786524b230eeca4f769f9dafcded
vbautistacode <vitorfbautista@gmail.com>
2025-05-08 12:31:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -517,7 +517,7 @@ with tab4:
 
         st.write(f"💰 **Total de Aposta:** R$ {df_cavalos_filtrado['Dutching Bet'].sum():.2f}")
         st.write(f"💸 **Gain Esperado:** R$ {df_cavalos_filtrado['Gain Dutch'].iloc[0]:.2f}")
-        lucro_aposta = df_cavalos_filtrado["Dutching Bet"].sum() - df_cavalos_filtrado["Gain Dutch"].iloc[0]
+        lucro_aposta = df_cavalos_filtrado["Gain Dutch"].iloc[0] - df_cavalos_filtrado["Dutching Bet"].sum()
         st.write(f"✅ **Lucro:** R$ {lucro_aposta:.2f}")
         
         st.divider()
